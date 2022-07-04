b41784868a31393bfcc9056b678e92e1ed87b738
Снежана Яровенко <snecanakaneva@gmail.com>
2022-07-04 18:17:08 +0300

    Webdriver install is fixed

--- conftest.py
+++ conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     yield driver
     driver.quit()
+    
+    

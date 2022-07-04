aafbcb33b4bb6b50661acce616584f4689dc6aab
Снежана Яровенко <snecanakaneva@gmail.com>
2022-07-04 22:54:11 +0300

    Element_present func is added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.common.exceptions import *
+
+
 class BasePage:
     
     def __init__(self, driver, url):
@@ -8,3 +11,10 @@ class BasePage:
         
     def open(self):
         self.driver.get(self.url)
+        
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True

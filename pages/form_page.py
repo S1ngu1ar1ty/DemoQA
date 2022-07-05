813bfef4ee424bebb753a92e84b411059c10b859
Снежана Яровенко <snecanakaneva@gmail.com>
2022-07-05 22:24:33 +0300

    Should_be_submit_btn func is created

--- pages/form_page.py
+++ pages/form_page.py
@@ -1,7 +1,11 @@
 from .base_page import BasePage
+from helpers.locators import *
 
 
 class FormPage(BasePage):
     
-    def __init__(self, *args, **kwargs):
-        super(FormPage, self).__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     super(FormPage, self).__init__(*args, **kwargs)
+    
+    def should_be_submit_btn(self):
+        assert self.is_element_present(*FormPageLocators.SUBMIT_BTN), 'There`s no submit btn on the form page'
